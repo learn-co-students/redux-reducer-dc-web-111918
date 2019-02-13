621f552ff51c2b5fae5f5edd32c8117087942b99
Manon Pilaud <mandyypilaud@gmail.com>
2019-02-13 10:43:46 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -5,14 +5,12 @@ export function manageFriends(state, action){
 
     case "REMOVE_FRIEND":
     const index = state.friends.findIndex(friend=> friend.id === action.id)
-    return (
-       {...state,
-           friends: [
-             ...state.friends.slice(0, index),
-             ...state.friends.slice(index + 1)
-           ]
-       }
-    )
+       let friendsCopy = [...state.friends]
+       friendsCopy.splice(index, 1)
+       return (
+           {...state,
+               friends: friendsCopy}
+       )
     default:
       return state;
   }
